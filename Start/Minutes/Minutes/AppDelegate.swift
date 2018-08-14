a453c9c6cc752c1aa11a6f925c60f6b1b5d39954
Macbook Pro <miguel.jimenez.0529@gmail.com>
2018-08-13 20:56:09 -0500

    Added App center frameworks

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,9 @@
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterPush
+import AppCenterDistribute
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("f385fcf8-9943-4a3b-ae31-bf6e15f0a353", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self,
+            MSPush.self,
+            MSDistribute.self
+            ])
         return true
     }
 
